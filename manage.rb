fa8379e7a1c64ab0ea8316e6562de33cc13cb2d1
Luca Marturana <lucamarturana@gmail.com>
2014-07-03 15:53:56 +0200

    Add manage script

---
+++ manage.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+require 'redis'
+
+HOSTS = { :redis01 => "192.168.57.2",
+          :redis02 => "192.168.57.3",
+          :redis03 => "192.168.57.4"
+}
+
+CONNECTIONS = HOSTS.map do |k, v|
+  Redis.new host: v
+end
+
+$sentinel = Redis.new host: HOSTS[:redis01], port: 26379
+
+case ARGV[0].to_sym
+when :setup
+  CONNECTIONS[:redis01].slaveof "NO", "ONE"
+  CONNECTIONS[:redis02].slaveof HOSTS[:redis01], 6379
+  CONNECTIONS[:redis03].slaveof HOSTS[:redis01], 6379
+when :failover
+  $sentinel.sentinel :failover, :test
+when :kill
+  ip, port = $sentinel.client.call %w(sentinel get-master-addr-by-name test)
+  r = Redis.new host: ip, port: port
+  r.client.call %w(debug segfault)
+when :restore
+  HOSTS.each do |k, v|
+    %x[vagrant ssh #{k} -- sudo service redis start]
+  end
+when :status
+  puts $sentinel.info['master0']
+end
